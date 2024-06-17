fd5292b80d2d779189046698450c4121580ca43a
Koeyh <gjcor96@naver.com>
2024-06-18 01:17:34 +0900

    초기 세팅
    jsp가 안열린다 !

---
+++ spring/mybatisboard/src/main/java/com/koeyh/mybatisboard/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.koeyh.mybatisboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    
+}
